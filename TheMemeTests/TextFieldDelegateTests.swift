a374d8e8d0bcc738def5baed7f06538f89e2071d
Isabella Bencardino <isabella@bencardino.com.br>
2022-04-05 19:32:34 +0100

    add test target

---
+++ TheMemeTests/TextFieldDelegateTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+
+@testable import TheMeme
+
+class TextFieldDelegateTests: XCTestCase {
+
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTextFieldDidBeginEditing_() {
+
+//GIVEN
+       
+
+//WHEN
+       
+//THEN
+       // textfield.text = ""
+
+    }
+}
